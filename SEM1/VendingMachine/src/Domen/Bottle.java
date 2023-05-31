eebc472f14da09a977de3cfc4aa169892c75e06e
Actav <hp@actav.com>
2023-05-31 21:10:24 +0300

    FIST COMMIT

---
+++ SEM1/VendingMachine/src/Domen/Bottle.java
@@ -0,0 +1,29 @@
+package Domen;
+
+public class Bottle extends Product {
+    private int volume;
+
+    public Bottle(int productId, String productName, String productCategory, double price, int volume) throws Exception {
+        super(productId, productName, productCategory, price);
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Product{" +
+                "name='" + super.getProductName() + '\'' +
+                "category='" + super.getProductCategory() + '\'' +
+                ", cost=" + super.getPrice() +
+                ", volume=" + volume +
+                '}';
+    }
+}
