73830540835d92c143dd5a746a63c0510df872b6
Actav <hp@actav.com>
2023-06-15 18:17:31 +0300

    SEM5 INIT COMMIT

---
+++ SEM5/MVCStudentApp/src/View/ViewEng.java
@@ -0,0 +1,26 @@
+package View;
+
+import Controller.iGetView;
+import Model.Student;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ViewEng implements iGetView {
+    public void printAllStudents(List<Student> students)
+    {
+        System.out.println("-----Displaying a list of students-----");
+        for(Student stud: students)
+        {
+            System.out.println(stud);
+        }
+        System.out.println("-----End of list-----");
+    }
+
+    @Override
+    public String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+}
